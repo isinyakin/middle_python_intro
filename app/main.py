9b5536a48ba74804f23f1909293ecfa6085f2e66
Igor Sinyakin <i.sinyakin@gmail.com>
2022-09-16 13:30:21 +0300

    Внесение правок в greeting и тесты, форматирование

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 """Генератор приветствий."""
-import pprint
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
+def greeting(name: str) -> str:
+    """Возвращает текст приветствия.
 
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    Args:
+        name: Имя пользователя
+
+    Returns:
+        str: Текст приветствия
+    """
+    greeting_name = name.lower().title()
+
+    return 'Привет, {0}'.format(greeting_name)
